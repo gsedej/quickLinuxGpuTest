037be51153722d941e5e06d8cd4efc687cf1f04d
gsedej <gsedej@gmail.com>
2014-03-12 23:15:05 +0100

    ver 0.4 - add some settings, lspci info, multi GPU detection, use of /tmp for workdir, offer saving results

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ quickLinuxGpuTest
 =================
 
 Simple BASH script to display basic GPU info and execute simple benchmarks
+Written and tested in Ubuntu 12.04
--- graphicsTest.sh
+++ graphicsTest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # by: Gašper Sedej
-# GPL3 lincence
-# ver 0.3
+# GPL-v3 lincence
+# ver 0.4, 2012-07-31
 
 
 if [ $# -ne 1 ] ; then
@@ -10,83 +10,149 @@ if [ $# -ne 1 ] ; then
 	exit
 fi
 
-# variables
 sleepTime="1"
-#intention
-ind1="	"
-ind2="		"
+useGlmark2="n"
+useGlmark2es="n"
+rootFolder="/tmp/graphicsTest/$1"
+saveFolder="`pwd`/$1"
 
 
-echo "Path: `pwd`/$1"
-read -p "OK (y/n)?"
 
-if [ "$REPLY" == "y" ]; then
-	mkdir -p "$1"
+#echo "Path: `pwd`/$1"
+echo "Working path: $rootFolder"
 
-	echo "----------"
-	echo "Driver information:"
-	echo "----------"
-	echo "glxinfo:"
+# check if folder exists
+if [ -d $rootFolder ]; then
+	read -p "Folder already exists. Continue (y/n)?"
+	if [ "$REPLY" != "y" ]; then
+		"Exiting..."
+	fi
+fi
+
+
 	
-	glxfile="$1"/"glxinfo.out"
+mkdir -p "$rootFolder"
+
+
+echo -e "\n"
+echo "----------"
+echo "GPU:"
+echo "----------"
+#lspci | grep VGA
+lspci -nn > "$rootFolder"/"lspci.out"
+lspci | grep VGA | cut -f5- -d ' '
+#info about all graphics cards
+for I in `lspci |awk '/VGA/{print $1}'`;do lspci -v -s $I;done > "$rootFolder"/"lspci-gpu.out"
+echo "GPU memory:"
+for I in `lspci |awk '/VGA/{print $1}'`;do lspci -v -s $I | sed -n '/Memory.*, prefetchable/s/.*\[size=\([^]]\+\)\]/\1/p';done
+sleep $sleepTime
+
+
+echo -e "\n"
+echo "----------"
+echo "modinfo:"
+echo "----------"
+modFile="$rootFolder"/"modinfo.out"
+
+for I in `lspci |awk '/VGA/{print $1}'`;do lspci -v -s $I | grep -i "kernel driver" | awk '{print $NF}';done > /tmp/activeMod
+activeMod=`cat /tmp/activeMod`
+echo "Active kernel object: $activeMod"
+
+# nvidia has different mod name, at least on ubuntu
+if [ $activeMod == nvidia ] ; then
+	# ne morem ugotoviti kateri obstaja. skrijem error
+	modinfo "nvidia_current" "nvidia_current_updates" 2>&1 | grep -v "ERROR: modinfo:" | > $modFile
+	# nvidia-smi
+else
+	modinfo $activeMod > $modFile	
+fi
+echo "Modinfo:"
+cat $modFile | grep file
+sleep $sleepTime
+
+
+echo -e "\n"
+echo "----------"
+echo "OpenGL information:"
+echo "----------"
+
+glxin=`which glxinfo`
+if [ -n $glxin ]; then
+	glxfile="$rootFolder"/"glxinfo.out"
 	glxinfo > $glxfile
 	#echo "Direct rendering:"
 	cat $glxfile | grep "direct rendering"
+	# `cat $glxfile | grep "direct rendering"| awk '{print $3}'`
 	cat $glxfile | grep -i "opengl"
 	sleep $sleepTime
+else
+	echo "glxinfo not installed" 
+fi
+sleep $sleepTime
+
+
+echo -e "\n"
+echo "----------"
+echo "Graphical benchmarks:"
+echo "----------"
+
+if [ $useGlmark2 == "y" ]; then
+	echo "Benchmarking glmark2"
 	
-	echo "----------"
-	echo "modinfo:"
-	echo "----------"
-	
-	modFile="$1"/"modinfo.out"
-	activeMod=`lsmod | grep -e "i810" -e "i915" -e "r128" -e "radeon" -e "savage" -e "sis" -e "nouveau" -e "poulsbo" -e "fglrx" -e "nvidia" | awk '{print $1}'`
-	echo "Active kernel object: $activeMod"
-	#sleep $sleepTime
-	
-	if [ $activeMod == nvidia ] ; then
-		echo "nvidia with propretarely driver detected"
-		modinfo "nvidia_current" > $modFile
-		# nvidia-smi
-		echo -n "Total memory: "
-		nvidia-smi -q -d MEMORY | grep Total | awk '{print $3}'
-		#echo ""
+	# najprej preveri ce obstaja program
+	glm2=`which glmark2`
+	if [ -n $glm2 ]; then
+		gl2file="$rootFolder"/"glmark2.out"
+		echo "glmark2 (opengl2)"
+		sleep $sleepTime
+		glmark2 > "$gl2file"
+		cat "$gl2file" | grep "Score"
 	else
-		modinfo $activeMod > $modFile
+		echo "glmark2 not found"
+	fi
+
+else
+	echo "Skipping glmark2..."
+fi
+
+
+if [ $useGlmark2es == "y" ]; then
+	echo "Benchmarking glmark2-es"
+	if [ -n $glm2 ]; then
+		gl2esfile="$rootFolder"/"glmark2-es2.out"
+		echo "glmark2-es2 (OpenGL ES2.0)"
+		sleep $sleepTime
+		glmark2-es2 > "$gl2esfile"
+		cat "$gl2esfile" | grep "Score"
+	else
+		echo "glmark2-es not found"
+	fi
+else
+	echo "Skipping glmark2-es..."
+fi
+
+sleep $sleepTime
+echo -e "\n"
+echo "----------"
+echo "Saving logs..."
+echo "----------"
+dmesg > "$rootFolder"/"dmesg.out"
+cat "/var/log/syslog" > "$rootFolder"/"syslog.out"
+cat "/var/log/Xorg.0.log" > "$rootFolder"/"Xorg.0.log.out"
+
+echo "Test ended!"
+
+read -p "Save results to $saveFolder/? (y/n)"
+if [ "$REPLY" == "y" ]; then
+	if [ -d $saveFolder ]; then
+		read -p "Directory exists. Overwrite? (y/n)"
+		if [ "$REPLY" != "y" ]; then
+			echo "Exiting without saving"
+			exit
+		fi
 		
 	fi
-	cat $modFile | grep file
-	
-	
-	echo "GPU:"
-	#lspci | grep VGA
-	lspci -nn > "$1"/"lspci.out"
-	lspci | grep VGA | cut -f2- -d ' '
-	# gpu memory
-	#for I in `lspci |awk '/VGA/{print $1}'`;do lspci -v -s $I;done | sed -n '/Memory.*, prefetchable/s/.*\[size=\([^]]\+\)\]/\1/p'
-		
-	echo "----------"
-	echo "Graphical benchmarks:"
-	echo "----------"
-	gl2file="$1"/"glmark2.out"
-	gl2esfile="$1"/"glmark2-es2.out"
-	
-	echo "glmark2 (opengl2)"
-	sleep $sleepTime
-	glmark2 > "$gl2file"
-	cat "$gl2file" | grep "Score"
-	
-	#echo "glmark2-es2 (OpenGL ES2.0)"
-	#sleep $sleepTime
-	#glmark2-es2 > "$gl2esfile"
-	#cat "$gl2esfile" | grep "Score"
-	
-	echo "----------"
-	echo "Saving logs..."
-	echo "----------"
-	dmesg > "$1"/"dmesg.out"
-	cat "/var/log/syslog" > "$1"/"syslog.out"
-	cat "/var/log/Xorg.0.log" > "$1"/"Xorg.0.log.out"
+	echo "saving results... "
 fi
 
 
